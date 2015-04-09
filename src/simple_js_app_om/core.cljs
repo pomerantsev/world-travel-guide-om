1b5fd1d1609eaaad743e59f91eea9ae9dee86784
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-04-09 14:10:28 +0300

    Change visibility at the right moments

--- src/simple_js_app_om/core.cljs
+++ src/simple_js_app_om/core.cljs
@@ -12,28 +12,34 @@
 (defonce app-state (atom {}))
 
 (defn update-country [owner]
+  (om/set-state! owner :visible false)
   (let [country-name (.country (js/Chance.) #js {:full true})
         encoded-country-name (js/encodeURIComponent country-name)]
     (go (let [response (<! (http/get
                              (str "https://country-images.herokuapp.com/image?q="
                                   encoded-country-name)
                              {:with-credentials? false}))]
-          (om/set-state! owner :country-name country-name)
-          (om/set-state! owner :image-src (:url (:body response)))))))
+          (om/set-state! owner :image-src "")
+          (js/requestAnimationFrame
+            (fn []
+              (om/set-state! owner :country-name country-name)
+              (om/set-state! owner :image-src (:url (:body response)))
+              (om/set-state! owner :visible true)))))))
 
 (defn app-view [data owner]
   (reify
     om/IInitState
     (init-state [_]
       {:country-name ""
-       :image-src ""})
+       :image-src ""
+       :visible false})
     om/IWillMount
     (will-mount [_]
       (update-country owner))
     om/IRenderState
     (render-state [this state]
       (dom/div nil
-        (dom/div nil
+        (dom/div #js {:style #js {:opacity (if (:visible state) 1 0)}}
           (dom/h2 nil (:country-name state))
           (dom/div nil
             (dom/img #js {:className "country-image"
